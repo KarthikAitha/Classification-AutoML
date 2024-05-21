ed7bd4ce74b224341564e1cf50321bfc9dc3e3e9
Karthik <karthikchandra494@gmail.com>
2024-05-21 11:46:57 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,46 +1,33 @@
-from operator import index
 import streamlit as st
-import plotly.express as px
-#from pycaret.regression import setup, compare_models, pull, save_model, load_model
-from pycaret.classification import setup, compare_models, pull, save_model, load_model
-import pandas_profiling
+from pycaret.classification import setup, compare_models, pull, save_model
 import pandas as pd
-from streamlit_pandas_profiling import st_profile_report
-import os 
 
-if os.path.exists('./dataset.csv'): 
-    df = pd.read_csv('dataset.csv', index_col=None)
+def main():
+    st.title("Auto ML Web Application")
 
-with st.sidebar: 
-    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
-    st.title("AutoML")
-    choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
-    st.info("This application helps you build and explore your data.")
+    # Upload data file
+    uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
-if choice == "Upload":
-    st.title("Upload Your Dataset")
-    file = st.file_uploader("Upload Your Dataset")
-    if file:
-        df = pd.read_csv(file, index_col=None)
-        df.to_csv('dataset.csv', index=None)
-        st.dataframe(df)
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.write("Data Preview:")
+        st.write(df.head())
 
-if choice == "Profiling": 
-    st.title("Exploratory Data Analysis")
-    profile_df = df.profile_report()
-    st_profile_report(profile_df)
 
-if choice == "Modelling": 
-    chosen_target = st.selectbox('Choose the Target Column', df.columns)
-    if st.button('Run Modelling'): 
-        setup(df, target=chosen_target, silent=True)
-        setup_df = pull()
-        st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
-        st.dataframe(compare_df)
-        save_model(best_model, 'best_model')
+        st.subheader("Modelling")
+        chosen_target = st.selectbox('Choose the Target Column', df.columns)
+        if st.button('Run Modelling'):
+                # Try running setup without silent=True to get more detailed errors
+            setup(df, target=chosen_target)
+            setup_df = pull()
+            st.dataframe(setup_df)
+            best_model = compare_models()
+            compare_df = pull()
+            st.dataframe(compare_df)
 
-if choice == "Download": 
-    with open('best_model.pkl', 'rb') as f: 
-        st.download_button('Download Model', f, file_name="best_model.pkl")
+            if st.button('Save Model'):
+                save_model(best_model, 'best_model')
+                st.success('Model saved successfully!')
+
+if __name__ == "__main__":
+    main()
